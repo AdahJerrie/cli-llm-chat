2e4e1c293e14b91f3519e08be78beffedcc306fa
Jerrie <adahjeremiah3@gmail.com>
2026-07-17 12:22:29 +0100

    feat: marshaling request

--- llm/client.go
+++ llm/client.go
@@ -1,6 +1,8 @@
 package llm
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -9,10 +11,41 @@ type Client struct {
 	httpClient *http.Client
 }
 
-// a constructor that creates the client from scratch
+// create the generate structs
+type GenerateRequest struct {
+	Model  string `json:"model"`
+	Prompt string `json:"prompt"`
+	Stream bool   `json:"stream"`
+}
+type GenerateResponse struct {
+	Model      string `json:"model"`
+	Created_at string `json:"created_at"`
+	Response   string `json:"response"`
+	Done       bool   `json:"done"`
+}
+
+// a constructor func that creates the client from scratch
 func NewClient(baseURL string) *Client {
 	return &Client{
 		baseURL:    baseURL,
 		httpClient: &http.Client{},
 	}
+}
+
+// the generate method: this compiles the needed request to the llm and the response from the llm
+func (c *Client) Generate(prompt string) (string, error) {
+
+	//build request struct from the prompt
+	reqBody := GenerateRequest{
+		Model:  "llama3",
+		Prompt: prompt,
+		Stream: false,
+	}
+
+	//marshal it into json bytes
+	dataByte, err := json.Marshal(reqBody)
+	if err != nil {
+		return "", fmt.Errorf("encoding request body: %w", err)
+	}
+	return "", nil
 }
