d438a7dcc0ee492ccb829e55d86e2fadbaa2a57b
AdahJerrie <adahjerry51@gmail.com>
2026-07-19 19:36:14 +0100

    feat: configuration setup

--- config/config.go
+++ config/config.go
@@ -13,18 +13,18 @@ type Config struct {
 }
 
 // build/load the configuration from environment variables or a configuration file.
-func LoadConfig() (Config, error) {
+func LoadConfig() (*Config, error) {
 	baseURL := os.Getenv("BASE_URL")
 	model := os.Getenv("MODEL")
 
 	if baseURL == "" {
-		return Config{}, errors.New("BASE_URL environment variable is required")
+		return nil, errors.New("BASE_URL environment variable is required")
 	}
 	if model == "" {
-		return Config{}, errors.New("MODEL environment variable is required")
+		return nil, errors.New("MODEL environment variable is required")
 	}
 
-	return Config{
+	return &Config{
 		BaseURL: baseURL,
 		Model:   model,
 		APIKey:  os.Getenv("API_KEY"),
