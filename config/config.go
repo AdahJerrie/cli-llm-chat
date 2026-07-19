29c0fa82348ea1d12c0023089e2be3c33b13c578
AdahJerrie <adahjerry51@gmail.com>
2026-07-19 04:39:11 +0100

    feat: configuration setup

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"errors"
+	"os"
+)
+
+// config struct defines the configuration for the application.
+type Config struct {
+	BaseURL string
+	Model   string
+	APIKEY  string
+}
+
+// build/load the configuration from environment variables or a configuration file.
+func LoadConfig() (Config, error) {
+	if os.Getenv("BASE_URL") == ""{
+		return Config{}, errors.New("BASE_URL environment variable is required")
+	}
+	if os.Getenv("MODEL") == "" {
+	 	return Config{}, errors.New("MODEL environment variable is required")
+	}
+
+	if os.Getenv("API_KEY") == ""{
+		return Config{}, errors.New("API_KEY environment variable is required")
+	}
+
+	return Config{
+		BaseURL: os.Getenv("BASE_URL"),
+		Model:   os.Getenv("MODEL"),
+		APIKEY:  os.Getenv("API_KEY"),
+	}, nil
+}
